17c11d41db4226c2777b7068e6bf3554cdb5363b
Rohan I Sunder <RohanImmanuel@users.noreply.github.com>
2016-06-02 00:45:50 +0530

    Czech Republic (Czechia) added and formating

--- js/data.js
+++ js/data.js
@@ -5,89 +5,89 @@ var data = {
 		youtube: "https://www.youtube.com/embed/ipVw772hCrM"
   	},
   	AL: {
-    	name: "Albania",
-    	continent: "Europe",
-    	youtube: "https://www.youtube.com/embed/9WZ06pTuqV0"
+	    	name: "Albania",
+	    	continent: "Europe",
+	    	youtube: "https://www.youtube.com/embed/9WZ06pTuqV0"
   	},
   	DZ: {
-    	name: "Algeria",
-    	continent: "Africa",
-    	youtube: "https://www.youtube.com/embed/YLY7oB_fttM"
+	    	name: "Algeria",
+	    	continent: "Africa",
+	    	youtube: "https://www.youtube.com/embed/YLY7oB_fttM"
   	},
   	AD: {
-    	name: "Andorra",
-    	continent: "Europe",
-    	youtube: "https://www.youtube.com/embed/POOvmkhlUD4"
+	    	name: "Andorra",
+	    	continent: "Europe",
+	    	youtube: "https://www.youtube.com/embed/POOvmkhlUD4"
   	},
   	AO: {
-    	name: "Angola",
-    	continent: "Africa",
-    	youtube: "https://www.youtube.com/embed/4QSmRYQBfN4"
+	    	name: "Angola",
+	    	continent: "Africa",
+	    	youtube: "https://www.youtube.com/embed/4QSmRYQBfN4"
   	},
   	AG: {
-    	name: "Antigua and Barbuda",
-    	continent: "North America",
-    	youtube: "https://www.youtube.com/embed/5OpiUD3qtY8"
+	    	name: "Antigua and Barbuda",
+	    	continent: "North America",
+	    	youtube: "https://www.youtube.com/embed/5OpiUD3qtY8"
   	},
   	AR: {
-    	name: "Argentina",
-    	continent: "South America",
-    	youtube: "https://www.youtube.com/embed/1U0N4SGL46A"
+	    	name: "Argentina",
+	    	continent: "South America",
+	    	youtube: "https://www.youtube.com/embed/1U0N4SGL46A"
   	},
   	AM: {
-    	name: "Armenia",
-    	continent: "Asia",
-    	youtube: "https://www.youtube.com/embed/sL4JK_bDo0A"
+	    	name: "Armenia",
+	    	continent: "Asia",
+	    	youtube: "https://www.youtube.com/embed/sL4JK_bDo0A"
   	},
   	AU: {
-    	name: "Australia",
-    	continent: "Oceania",
-    	youtube: "https://www.youtube.com/embed/ynHIlx5RgtI"
+	    	name: "Australia",
+	    	continent: "Oceania",
+	    	youtube: "https://www.youtube.com/embed/ynHIlx5RgtI"
   	},
   	AT: {
-    	name: "Austria",
-    	continent: "Europe",
-    	youtube: "https://www.youtube.com/embed/hKsGmyhsKFA"
+	    	name: "Austria",
+	    	continent: "Europe",
+	    	youtube: "https://www.youtube.com/embed/hKsGmyhsKFA"
   	},
   	AZ: {
 		name: "Azerbaijan",
-    	continent: "Asia",
-    	youtube: "https://www.youtube.com/embed/DqVPVRmRIU8"
+    		continent: "Asia",
+    		youtube: "https://www.youtube.com/embed/DqVPVRmRIU8"
   	},
   	BS: {
 		name: "Bahamas",
-    	continent: "North America",
-    	youtube: "https://www.youtube.com/embed/J9aV4Zn8JJE"
+    		continent: "North America",
+    		youtube: "https://www.youtube.com/embed/J9aV4Zn8JJE"
   	},
   	BH: {
-    	name: "Bahrain",
-    	continent: "Asia",
-    	youtube: "https://www.youtube.com/embed/P2gHUcwZbYk"
+	    	name: "Bahrain",
+	    	continent: "Asia",
+	    	youtube: "https://www.youtube.com/embed/P2gHUcwZbYk"
   	},
   	BD: {
-    	name: "Bangladesh",
-    	continent: "Asia",
-    	youtube: "https://www.youtube.com/embed/ljebhxk_9ys"
+	    	name: "Bangladesh",
+	    	continent: "Asia",
+	    	youtube: "https://www.youtube.com/embed/ljebhxk_9ys"
   	},
   	BB: {
-    	name: "Barbados",
-    	continent: "North America",
-    	youtube: "https://www.youtube.com/embed/uvA3DUJCxpY"
+	    	name: "Barbados",
+	    	continent: "North America",
+	    	youtube: "https://www.youtube.com/embed/uvA3DUJCxpY"
   	},
   	BY: {
-    	name: "Belarus",
-    	continent: "Europe",
+	    	name: "Belarus",
+	    	continent: "Europe",
 		youtube: "https://www.youtube.com/embed/nASqSOtXkhk"
 	},
 	BE: {
-    	name: "Belgium",
-    	continent: "Europe",
-    	youtube: "https://www.youtube.com/embed/0TuMvWCbM-g"
+	    	name: "Belgium",
+	    	continent: "Europe",
+	    	youtube: "https://www.youtube.com/embed/0TuMvWCbM-g"
   	},
   	BZ: {
-    	name: "Belize",
-    	continent: "North America",
-    	youtube: "https://www.youtube.com/embed/3MrYk3kCUGk"
+	    	name: "Belize",
+	    	continent: "North America",
+	    	youtube: "https://www.youtube.com/embed/3MrYk3kCUGk"
   	},
   	BJ: {
 		name: "Benin",
@@ -213,5 +213,10 @@ var data = {
 		name: "Cyprus",
 		continent: "Europe",
 		youtube: "https://www.youtube.com/embed/4w_Vc_7irhM"
+  	},
+  	CZ: {
+  		name: "Czech Republic (Czechia)",
+  		continent: "Europe",
+		youtube: "https://www.youtube.com/embed/kaF6SnSEo8"
   	}
 }
